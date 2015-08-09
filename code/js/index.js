e707dffc747ec8eb717d7e42554629d8a3f5271f
kylehalstead <halsteadkyle@gmail.com>
2015-08-09 14:48:51 -0400

    added comments to wanted JS fuctions

--- code/js/index.js
+++ code/js/index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-  alert("hello")
+  //alert("hello")
 });
+
+
+/* action I do want.
+(read more to expand "story" section)
+icon to inlarge slightly on hover
+main nav header to change colors when scrolling and stick at top for whole page
+in nav when anchor point in reached within scroll that li item in nav should inlarge
+some interested landing page logo exit when scolling  */
