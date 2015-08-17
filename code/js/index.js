c97f816477100c49fae0791c46a14f43e05ef4f2
kylehalstead <halsteadkyle@gmail.com>
2015-08-17 00:23:44 -0400

    added buttons to products, footer logo

--- code/js/index.js
+++ code/js/index.js
@@ -25,6 +25,15 @@ $(document).ready(function() {
 
 });
 
+/* someone elses sample for full scrren div on click $('div').click(function() {
+  $(this).css({
+      position:'absolute', //or fixed depending on needs
+      top: $(window).scrollTop(), // top pos based on scoll pos
+      left: 0,
+      height: '100%',
+      width: '100%'
+  } */
+
 
 /* action I  want.
 (read more to expand "story" section)
